7e0a65cd663454e5973fac71d4e20f24278736b2
Shailesh Prajapati <Shailesh.Prajapati@einfochips.com>
2023-02-10 11:22:41 +0530

    Activity for day3

---
+++ Day3/InningsMain.java
@@ -0,0 +1,29 @@
+
+
+import java.util.Scanner;
+
+public class InningsMain {
+
+	public static void main(String[] args) {
+		
+		
+		  Scanner sc = new Scanner(System.in);
+	        System.out.println("Enter the team name: ");
+	        String teamName = sc.nextLine();
+	        System.out.println("Enter session: ");
+	        String innigsName = sc.nextLine();
+	        if((!innigsName.toLowerCase().equals("first")) && (!innigsName.toLowerCase().equals("second")))
+	        {
+	            System.out.println("Invalid input, session can be first or second only");
+	            return;
+	        }
+	        System.out.println("Enter runs :");
+	        int runs = sc.nextInt();
+
+	        Innings innings = new Innings(teamName, innigsName.toLowerCase(), runs);
+
+	        innings.displayInningsDetails();
+
+	}
+
+}
