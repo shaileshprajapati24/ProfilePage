ced0f1f6cd5d39f268cc27b0b132f01fa72b4e17
Shailesh Prajapati <Shailesh.Prajapati@einfochips.com>
2023-02-15 16:17:00 +0530

    Ex07 commit

---
+++ Ex07/setActivity.java
@@ -0,0 +1,28 @@
+package Ex07;
+
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class setActivity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		TreeSet<String> ts = new TreeSet<String>();
+		
+		System.out.println("Enter number of player");
+		int n = sc.nextInt();
+		
+		System.out.println("Enter  "+n+" player name");
+		for(int i=0; i<=n; i++)
+		{
+		      ts.add(sc.nextLine());	
+		}
+		
+          System.out.println(ts);
+        
+
+	}
+
+}
