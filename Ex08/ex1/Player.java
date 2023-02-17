9b75c818f4f8a792362f9cc6c41d99e2f983fd9a
Shailesh Prajapati <Shailesh.Prajapati@einfochips.com>
2023-02-17 10:46:06 +0530

    comparator commit of Ex08 Activity

---
+++ Ex08/ex1/Player.java
@@ -0,0 +1,32 @@
+package Ex08.ex1;
+
+public class Player {
+	private String name;
+	private String skill;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSkill() {
+		return skill;
+	}
+	public void setSkill(String skill) {
+		this.skill = skill;
+	}
+	public Player(String name, String skill) {
+		super();
+		this.name = name;
+		this.skill = skill;
+	}
+	public Player() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Main name=" + name + ", skill=" + skill ;
+	}
+
+}
